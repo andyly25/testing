20c4ef4d9ee67477da10ba0dad112e6781a872ff
Andy Ly <andyly25@yahoo.com>
2016-05-03 16:53:20 -0700

    added comments so that I can come back to this and understand better

--- MongoDB/example2.js
+++ MongoDB/example2.js
@@ -3,17 +3,30 @@
 var mongodb = require('mongodb');
 
 var uri = 'mongodb://localhost:27017/example';
+/* MongoClient helps you create a connection to MongoDB
+ * .connect function takes connection string which tells driver which 
+ * MongoDB to connect to, and this is to the example db
+ * It also takes a callback function (for errors or successful connection)
+ */
 mongodb.MongoClient.connect(uri, function(error, db){
 	if (error){
 		console.log(error);
 		process.exit(1);
 	}
-
+	/* Once has a db handle, can then access collection function
+	 * With collection, you can insert a document  
+	 */
 	db.collection('sample').insert({x:1}, function(error, result){
 		if (error){
 			console.log(error);
 			process.exit(1);
 		}
+		/* Query for a document is trikeier since if yo pass call back to 
+		 * driver's find cuntion, you'll get back a cursor rather than set of documents.
+		 * cursor: object that you could call next on to get the next document
+		 * the driver has the toarray function that exhausts cursor and
+		 * returns an array of documents in the callback
+		 */
 		db.collection('sample').find().toArray(function(error,docs){
 			if(error){
 				console.log(error);
