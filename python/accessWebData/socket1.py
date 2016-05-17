1c8c2d1ee5a9ea96741d5225b414cda4828bb7fe
Andy Ly <andyly25@yahoo.com>
2016-05-16 18:01:39 -0700

    small update

---
+++ python/accessWebData/socket1.py
@@ -0,0 +1,15 @@
+# This is from a tutorial
+# Note python3 will throw errors, so use python2
+import socket
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('www.pythonlearn.com', 80))
+mysock.send ('GET http://www.pythonlearn.com/code/intro-short.txt HTTP/1.0\n\n')
+
+while True:
+	data = mysock.recv(512)
+	if(len(data)<1):
+		break
+	print(data)
+mysock.close()
+
+# Yay, you made a web browser?
---
+++ python/accessWebData/urllib1.py
@@ -0,0 +1,12 @@
+# Does not give headers, just body/content
+
+import urllib
+fhand = urllib.urlopen('http://www.py4inf.com/code/romeo.txt')
+
+counts = dict()
+for line in fhand:
+	words = line.split()
+	for word in words:
+		counts[word] = counts.get(word,0)+1
+
+print counts
